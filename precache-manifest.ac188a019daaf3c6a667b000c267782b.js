c8000186e3393b2a1169d55e691750855802e3ef
Runningwilder <languageblog96@gmail.com>
2020-11-12 17:35:26 +0200

    Updates

---
+++ precache-manifest.ac188a019daaf3c6a667b000c267782b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2311590bda97a87eea69e3e0985e67e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "385a589038b8cf254f54",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3ff1766470b599d42b15",
+    "url": "/static/js/2.0cda6933.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0cda6933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385a589038b8cf254f54",
+    "url": "/static/js/main.841fdb6b.chunk.js"
+  },
+  {
+    "revision": "de0170739274e187600c",
+    "url": "/static/js/runtime-main.c1f0c216.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
